46d12cd9310c179281712f3d028054c6d5ccb76e
Bishwambar11 <bishwambarchakma@gmail.com>
2026-05-18 21:56:58 +0000

    hello

---
+++ New.js
@@ -0,0 +1,14 @@
+let x =2;
+let y=5;
+
+console.log(x + y);
+
+ myVariable 
+myvariable ="hello";
+let word = "goodbye"
+
+console.log(myVariable);
+
+let isQueenElizabethDead =true
+
+console.log(isQueenElizabethDead);
---
+++ New2.js
@@ -0,0 +1,31 @@
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+//  console.log(add(2, 5));
+//  console.log(add(50, 50));
+
+//  const sum = add(3, 8);
+//  console.log(sum >10)
+
+
+
+function subtract(num1,num2) {
+    return num1 - num2;
+}
+console.log(subtract(2, 5));
+console.log(subtract(50, 50));
+
+
+
+function square(num) {
+    return num * num;
+}
+ 
+console,og(square(3))
+
+funtion greet (name) {
+    retrn "Hollo, " 
+}
+
